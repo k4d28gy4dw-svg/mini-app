0729264c48a8d018192a74892605f2bb58f3d8e1
k4d28gy4dw-svg <k4d28gy4dw@privaterelay.appleid.com>
2026-07-10 06:04:45 +0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,3 +1,1 @@
-// Вставьте сюда данные из Supabase → Project Settings → API
-window.SUPABASE_URL = "PASTE_SUPABASE_URL_HERE";
-window.SUPABASE_ANON_KEY = "PASTE_SUPABASE_ANON_KEY_HERE";
+Unsupported Media Type
