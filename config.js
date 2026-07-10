ef28cf1bf5375a04cba58202c6d7cae71852e17e
k4d28gy4dw-svg <k4d28gy4dw@privaterelay.appleid.com>
2026-07-10 06:49:27 +0300

    Configure Supabase

--- config.js
+++ config.js
@@ -1,1 +1,4 @@
-Unsupported Media Type
+// Публичные данные проекта Supabase: Project Settings -> API.
+// Anon key можно использовать в браузере при включённом RLS.
+window.SUPABASE_URL = "https://ytsmegudftfudvvgghor.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_gLALPxs3JtOYV2MZCGEtkA_27WXvQrl";
